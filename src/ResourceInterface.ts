bb682da39582b4dd8dae73d3b1bff4a4d8d0a1b6
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-15 12:08:31 +0100

    created resource interface

---
+++ src/ResourceInterface.ts
@@ -0,0 +1,12 @@
+export interface Resource {
+    resource_name: string;
+    author_name: string;
+    url: string;
+    description: string;
+    tags: string;
+    content_type: string;
+    recommended_stage: string;
+    user_id: number;
+    creator_opinion: string;
+    creator_reason: string;
+}
